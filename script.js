1204585230d6706ad6605328b49706a2b84ee1c5
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-28 10:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+ 
+    function getRandomTimeout() {
+      return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+    }
+
+    const promises = [
+      new Promise(resolve => setTimeout(() => resolve('Promise 1 resolved'), getRandomTimeout())),
+      new Promise(resolve => setTimeout(() => resolve('Promise 2 resolved'), getRandomTimeout())),
+      new Promise(resolve => setTimeout(() => resolve('Promise 3 resolved'), getRandomTimeout())),
+      new Promise(resolve => setTimeout(() => resolve('Promise 4 resolved'), getRandomTimeout())),
+      new Promise(resolve => setTimeout(() => resolve('Promise 5 resolved'), getRandomTimeout()))
+    ];
+
+    Promise.any(promises)
+      .then(result => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerText = result;
+      })
+      .catch(error => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerText = 'All promises rejected: ' + error;
+      });
+
